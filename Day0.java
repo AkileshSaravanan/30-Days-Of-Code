de4fc389df982af94b3abef9eefd2fd3d4207552
AKILESH S <rmsakilesh23@gmail.com>
2023-06-13 20:42:00 +0530

    initial commit

---
+++ Day0.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Day0 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String inputString = s.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
