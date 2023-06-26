816b2bf82042830138c28a9e299d04493032ed4f
AKILESH S <rmsakilesh23@gmail.com>
2023-06-26 17:10:56 +0530

    initial commit

---
+++ Day16/Day16.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day16 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+    String S=sc.nextLine();
+    try
+    {
+        int no;
+        no=Integer.parseInt(S);
+        System.out.println(no);
+    }
+    catch(Exception a)
+    {
+        System.out.println("Bad String");
+    }          
+   }
+  }
